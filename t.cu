9bafe40edd82dcb4a26ef2fd99d5632188d4d72d
guishuo <997726077@qq.com>
2023-05-17 14:49:35 +0800

    fix time

--- t.cu
+++ t.cu
@@ -81,7 +81,7 @@ int main() {
 	}
 	end = clock();
 	// printf("(CPU) sum = %d, using time: %ld\n", sum, time_used);
-	printf("(CPU) sum = %d\n, using time: %lf ms\n", sum, (double)(end - start)/CLOCKS_PER_SEC*1000);
+	printf("(CPU) sum = %d, using time: %lf ms\n", sum, (double)(end - start)/CLOCKS_PER_SEC*1000);
 
 	return 0;
 }

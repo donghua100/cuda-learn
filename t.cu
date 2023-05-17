16bbf1d3a6323a979bb6fd26322a9e59be5d9c8a
guishuo <997726077@qq.com>
2023-05-17 10:54:21 +0800

    cal time

--- t.cu
+++ t.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <cuda_runtime.h>
 
 #define DATA_SIZE 1024*1024
@@ -36,14 +37,12 @@ int init_cuda() {
 	return 0;
 }
 
-__global__ static void sumOfSquares(int *nums, int *result, clock_t *time) {
+__global__ static void sumOfSquares(int *nums, int *result) {
 	int sum = 0;
-	clock_t start = clock();
 	for (int i = 0; i < DATA_SIZE; i++) {
 		sum += nums[i] * nums[i];
 	}
 	*result = sum;
-	*time = clock() - start;
 }
 
 int main() {
@@ -54,36 +53,35 @@ int main() {
 		printf("initialized CUDA fail!\n");
 		return -1;
 	}
-
 	generate_nums(data, DATA_SIZE);
 
+	clock_t start = clock();
 	int *gpudata, *result;
-	clock_t *time;
 	cudaMalloc((void **)&gpudata, sizeof(int)*DATA_SIZE);
 	cudaMalloc((void **)&result, sizeof(int));
-	cudaMalloc((void **)&time, sizeof(clock_t));
 	cudaMemcpy(gpudata, data, sizeof(int)*DATA_SIZE, cudaMemcpyHostToDevice);
 
-	sumOfSquares<<<1,1,0>>>(gpudata, result, time);
+	sumOfSquares<<<1,1,0>>>(gpudata, result);
 
 	int sum;
-	clock_t time_used;
 	cudaMemcpy(&sum, result, sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(&time_used, time, sizeof(clock_t), cudaMemcpyDeviceToHost);
 	cudaFree(gpudata);
 	cudaFree(result);
-	cudaFree(time);
 
-	printf("sum = %d, using time: %ld\n", sum, time_used);
+	cudaDeviceSynchronize();
+	clock_t end = clock();
+
+
+	printf("(GPU) sum = %d, using time: %lf\n ms", sum, (double)(end - start)/CLOCKS_PER_SEC*1000);
 
 	sum = 0;
-	// clock_t start = clock();
+	start = clock();
 	for (int i = 0; i < DATA_SIZE; i++) {
 		sum += data[i] * data[i];
 	}
-	// time_used = clock() - start;
+	end = clock();
 	// printf("(CPU) sum = %d, using time: %ld\n", sum, time_used);
-	printf("(CPU) sum = %d\n", sum);
+	printf("(CPU) sum = %d\n, using time: %lf\n ms", sum, (double)(end - start)/CLOCKS_PER_SEC*1000);
 
 	return 0;
 }
